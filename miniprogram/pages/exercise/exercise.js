f5619477aa2c1994188e9b049ccd66f639dad86b
7ee <sevencifer@7ucifer.cn>
2021-03-22 22:31:43 +0800

    Initial Commit

---
+++ miniprogram/pages/exercise/exercise.js
@@ -0,0 +1,83 @@
+// pages/exercise/exercise.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+    
+    wx.setNavigationBarTitle({
+      title: ""//页面标题为路由参数
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  onShow: function() {
+    if (getCurrentPages().length != 0) {
+      //刷新当前页面的数据
+      getCurrentPages()[getCurrentPages().length - 1].onLoad()
+    }
+  },
+
+  //认单词
+  start: function() {
+    wx.navigateTo({
+      url: 'learn_vocab/learn_vocab',
+    })
+  },
+
+  //默单词
+  start_reverse: function() {
+    wx.navigateTo({
+      url: 'spell_vocab/spell_vocab',
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ miniprogram/pages/tabbar.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    "pagePath": "/pages/exercise/exercise",
+    "text": "练习",
+    "iconPath": "/images/tabbar/exercise.png",
+    "selectedIconPath": "/images/tabbar/exercise-select.png"
+  },
+  {
+    "pagePath": "/pages/vocabulary/vocabulary",
+    "text": "查词",
+    "iconPath": "/images/tabbar/vocabulary.png",
+    "selectedIconPath": "/images/tabbar/vocabulary-select.png"
+  },
+  {
+    "pagePath": "/pages/settings/settings",
+    "text": "设置",
+    "iconPath": "/images/tabbar/settings.png",
+    "selectedIconPath": "/images/tabbar/settings-select.png"
+  }
+]
